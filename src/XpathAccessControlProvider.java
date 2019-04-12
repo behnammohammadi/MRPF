00ab459e7b0a216d12347aed1f85961e7e0706d7
behnam <behnamm92@gmail.com>
2019-04-12 16:02:21 +0430

    WIP: on checkaccess add path summery support

--- src/XpathAccessControlProvider.java
+++ src/XpathAccessControlProvider.java
@@ -1,12 +1,21 @@
 import Dtos.AccessControlDto;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by behnam on 4/9/19.
  */
 public class XpathAccessControlProvider {
+
+    public static void main(String[] arguments)
+    {
+        try {
+            System.out.println(CheckAccess(Utilities.ReadFileAsString("data/AC.data"),"A/D[1]/G"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static boolean CheckAccess(String accessControl,String xpath) throws IOException {
 
         String [] lines=accessControl.split("\\r?\\n|\\r");
@@ -18,6 +27,14 @@ public class XpathAccessControlProvider {
             String path=line.split(" ")[1];
 
             accessList.add(new AccessControlDto(accessibility,path));
+
+            /*Collections.sort(accessList, new Comparator<>() {
+                public int compare(AccessControlDto o1, AccessControlDto o2) {
+                    if (o1.getPath().length() == o2.getPath().length())
+                        return 0;
+                    return o1.getPath().length() < o2.getPath().length() ? -1 : 1;
+                }
+            });*/
         }
 
        return Check(accessList,xpath);
@@ -28,10 +45,32 @@ public class XpathAccessControlProvider {
         if (xpath.equals(""))
             return false;
 
+        String [] mainXpathSplited=xpath.split("/");
         for (AccessControlDto item:accessList) {
-
+            String currentPath=item.getPath();
            if( item.getPath().equals(xpath))
                return item.getAccessibility();
+            else
+           {
+               int delimerLoc=item.getPath().lastIndexOf("/");
+               int bracketLoc=item.getPath().lastIndexOf("[");
+               if(bracketLoc>delimerLoc)
+               {
+                   String newXpath=item.getPath().substring(0,bracketLoc); //remvoe last ndoe index
+                   if( item.getPath().equals(newXpath))
+                       return item.getAccessibility();
+               }
+           }
+            /*String[] ItemPathSplited=item.getPath().split("/");
+            for(int i=0;i<=ItemPathSplited.length;i++)
+            {
+                if(mainXpathSplited[i]!=ItemPathSplited[i])
+                {
+
+                }
+            }*/
+
+
         }
 
         if (xpath.indexOf("/")==-1)
